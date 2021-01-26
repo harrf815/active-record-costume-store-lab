5130fed49ba6b73660564cee5f23af7149493bfb
Harrf Akbar <harrf_akbar@yahoo.com>
2021-01-25 19:27:53 -0800

    Done.

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,15 @@
 # Create your CostumeStore class here
+
+class CostumeStore < ActiveRecord::Base
+
+end
+# def change
+#     create_table :costume_stores do |t|
+#         t.string 'name'
+#         t.string 'location'
+#         t.integer 'costume inventory'
+#         t.integer 'number of employees'
+#         t.string 'whether or not its still business'
+#         t.datetime 'opening time'
+#         t.datetime 'closing time'
+#     end
--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,18 @@
 # Create your HauntedHouse class here
+
+class HauntedHouse < ActiveRecord::Base
+
+
+end
+# def change 
+#     create_table :haunted_houses do |t|
+#         t.string 'name'
+#         t.string 'location'
+#         t.string 'theme'
+#         t.integer 'price'
+#         t.string 'whether they are family friendly or not'
+#         t.datetime 'opening date'
+#         t.datetime 'closing date'
+#         t.string 'long decription'
+#     end
+# end
